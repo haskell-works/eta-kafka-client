bb853d5e69f728b3cd62c9fdac0f8e5a032d3c7b
Alexey Raga <alexey.raga@gmail.com>
2017-01-18 21:31:22 +1100

    init

---
+++ src/Kafka/Internal/Bindings.hs
@@ -0,0 +1,7 @@
+module Kafka.Internal.Bindings
+where
+
+import Java
+import Control.Monad(forM_)
+import Data.Map (Map)
+import qualified Data.Map as M
---
+++ src/Kafka/Internal/Collections.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE MagicHash, FlexibleContexts, DataKinds, TypeFamilies #-}
+module Kafka.Internal.Collections
+where
+
+import Java
+import Control.Monad(forM_)
+import Data.Map (Map)
+import qualified Data.Map as M
+
+data {-# CLASS "java.util.Map" #-} JMap k v = JMap (Object# (JMap k v))
+  deriving Class
+
+data {-# CLASS "java.util.HashMap" #-} JHashMap k v
+  = JHashMap (Object# (JHashMap k v))
+  deriving Class
+
+type instance Inherits (JHashMap k v) = '[Object, JMap k v]
+
+foreign import java unsafe "@new" newHashMap :: Java a (JHashMap k v)
+
+foreign import java unsafe "put"
+  jmapPut :: (Extends k Object, Extends v Object)
+          => k -> v -> Java (JHashMap k v) v
+
+toJMap :: (Class k, Class v) => Map k v -> JMap k v
+toJMap m = pureJava $ do
+  hm <- newHashMap
+  forM_ (M.assocs m) $ \(k, v) ->
+    hm <.> jmapPut k v
+  return (superCast hm)
