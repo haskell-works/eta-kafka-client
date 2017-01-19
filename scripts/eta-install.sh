b3846d564988e617e61e7f22a5fded0efd763609
Alexey Raga <alexey.raga@gmail.com>
2017-01-19 23:20:24 +1100

    cache eta

--- scripts/eta-install.sh
+++ scripts/eta-install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-if [ ! -d "~/.eta" ]; then
+if [ ! -d "~/eta" ]; then
   cd ~/
   git clone --recursive https://github.com/typelead/eta
   cd eta
   ./install.sh
 else
-  cd ~/.eta
+  cd ~/eta
   ./update.sh
 fi
